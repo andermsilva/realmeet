d72d6a264f00c2c2ea7434587232bd3889750760
andermsilva <andermarsil@gmail.com>
2023-02-18 10:34:01 -0300

    fix-3.0 build-spec.yml

--- service/src/main/java/br/com/sw2you/realmeet/RoomController.java
+++ service/src/main/java/br/com/sw2you/realmeet/RoomController.java
@@ -9,7 +9,6 @@ import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class RoomController implements RoomsApi {
 
