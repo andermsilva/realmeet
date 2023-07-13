3b31c1464712d1186f6890efea9cd1504d5a8067
andermsilva <andermarsil@gmail.com>
2023-07-13 17:53:37 -0300

    fix formatter RoomNotFoundException

--- service/src/main/java/br/com/sw2you/realmeet/exception/RoomNotFoundException.java
+++ service/src/main/java/br/com/sw2you/realmeet/exception/RoomNotFoundException.java
@@ -1,8 +1,10 @@
 package br.com.sw2you.realmeet.exception;
 
 public class RoomNotFoundException extends RuntimeException {
+
     public RoomNotFoundException(String message) {
         super(message);
     }
+
     public RoomNotFoundException() {}
 }
